c9a5e6ef78a3e92a1b655098016fee05d031de4f
Justin L <levayjustin@gmail.com>
2017-08-03 13:40:15 -0400

    Fixed deploy task w/ 2 outputs

--- build/tasks/deploy.js
+++ build/tasks/deploy.js
@@ -4,19 +4,22 @@ var forceDeploy = require('gulp-jsforce-deploy');
 var paths = require('../paths');
 var config = require('../sf-config');
 
-gulp.task('sf-zip', ['build'], function () {
-    return gulp.src([paths.output + '/**'])
-        .pipe(zip(config.package + '.resource'))
-        .pipe(gulp.dest([paths.staticresources, paths.projectStaticResources]))
+gulp.task('sf-zip', ['build'], function() {
+  gulp.src([paths.output + '/**']).pipe(zip(config.package + '.resource')).pipe(gulp.dest(paths.staticresources));
+
+  return gulp
+    .src([paths.output + '/**'])
+    .pipe(zip(config.package + '.resource'))
+    .pipe(gulp.dest(paths.projectStaticResources));
 });
 
-gulp.task('deploy', ['sf-zip'], function () {
-    return gulp.src([paths.package + '**/*'], { base: "." })
-        .pipe(zip('pkg.zip'))
-        .pipe(forceDeploy({
-            username: config.username,
-            password: config.password + config.token,
-            loginUrl: config.loginUrl,
-            pollTimeout: 120 * 1000
-        }));
+gulp.task('deploy', ['sf-zip'], function() {
+  return gulp.src([paths.package + '**/*'], { base: '.' }).pipe(zip('pkg.zip')).pipe(
+    forceDeploy({
+      username: config.username,
+      password: config.password + config.token,
+      loginUrl: config.loginUrl,
+      pollTimeout: 120 * 1000
+    })
+  );
 });
