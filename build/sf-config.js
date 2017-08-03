3601c22136e850a641b4bb35c1387a8a86f5c4ca
Justin L <levayjustin@gmail.com>
2017-08-03 13:25:13 -0400

    Missing field

--- build/sf-config.js
+++ build/sf-config.js
@@ -2,5 +2,6 @@ module.exports = {
   username: 'dev@aarp.org.dev',
   password: ' ',
   token: ' ',
-  loginUrl: 'https://test.salesforce.com'
+  loginUrl: 'https://test.salesforce.com',
+  package: ''
 }
